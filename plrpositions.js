6d19848f4fa61b6820744e25dff9d1b409c65913
Tiago Quintas <tjbquintas@gmail.com>
2023-12-18 13:06:37 +0000

    pessima mensagem de commit

--- plrpositions.js
+++ plrpositions.js
@@ -1,13 +1,15 @@
-
+var favs = {};
 var vm = function () {
     console.log('ViewModel initiated...');
     //---Variáveis locais
     var self = this;
+    var fav_records;
     self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Seasons');
     self.displayName = 'NBA Seasons List';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
     self.records = ko.observableArray([]);
+    self.favRecords = ko.observableArray([]);
     self.currentPage = ko.observable(1);
     self.pagesize = ko.observable(20);
     self.totalRecords = ko.observable(50);
@@ -56,8 +58,47 @@ var vm = function () {
             self.pagesize(data.PageSize)
             self.totalPages(data.TotalPages);
             self.totalRecords(data.TotalRecords);
+            fav_records = data.Records;
             console.log(data.Records);
         });
+    };
+
+    self.update = function() {
+        var lst = [];
+        for (var rec in favs) {
+            lst.push(favs[rec])
+        }
+        self.favRecords(lst);
+    }
+    self.recoverFavs = function() {
+        if (localStorage.getItem("plrpositions_favs")) {
+            favs = JSON.parse(localStorage.getItem("plrpositions_favs"))
+            for (var id in favs) {
+                $(`#${id}`).find("i").toggleClass("fa-heart text-danger");
+                $(`#${id}`).find("i").toggleClass("fa-heart-o");
+            }
+            self.update()
+        }
+    }
+    self.favReady = function() {
+        $('[id^="favourite_"]').on('click', e => {
+            $(e.target).find("i").toggleClass("fa-heart text-danger");
+            $(e.target).find("i").toggleClass("fa-heart-o");
+            var id = e.target.id;
+            if (id in favs) {
+                delete favs[id]
+            } else {
+                var rec_id = Number(id.slice(10))
+                for (var rec of fav_records) {
+                    if (rec.Id == rec_id) {
+                        favs[id] = rec;
+                        break;
+                    }
+                }
+            }
+            localStorage.setItem("plrpositions_favs", JSON.stringify(favs))
+            self.update()
+        })
     };
     
     //--- Internal functions
@@ -121,9 +162,10 @@ var vm = function () {
     console.log("VM initialized!");
 };
 
+var nvm;
 $(document).ready(function () {
     console.log("ready!");
-    var nvm = new vm();
+    nvm = new vm();
     ko.applyBindings(nvm);
 
     $(document).on('click', "[id^='favourite_']", event => {
@@ -238,17 +280,8 @@ function ajaxHelper(uri, method, data) {
 
 $(document).ajaxComplete(function (event, xhr, options) {
     $("#myModal").modal('hide');
-    
-    /*var favorites = localStorage.getItem("favorites")
-    favorites = JSON.parse(favorites);
-    console.log(favorites["Id"])
-    for (var fv in favorites["Id"]){
-        if (favorites["Id"][fv] != null){
-            item = 'favourite_'+favorites["Id"][fv]
-            $('#' + item + ' i').addClass("fa-heart text-danger").removeClass("fa-heart-o");
-            console.log("found " + item)
-        }
-    }*/
+    nvm.recoverFavs();
+    nvm.favReady();
 });
 
 
